1c3ce43bf8f6db35e9c8c002abcd0cf45990779b
Juan David Nicholls Cardona <juan.nicholls@yuxipacific.com>
2016-10-14 15:21:53 -0500

    Fix internal pages transition navigation

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,9 @@ var $container = $("#container");
 var outClassRight = 'pt-page-rotateSlideRightOut',
     inClassRight = 'pt-page-rotateSlideRightIn',
     outClassLeft = 'pt-page-rotateSlideOut',
-    inClassLeft = 'pt-page-rotateSlideIn';
+    inClassLeft = 'pt-page-rotateSlideIn',
+    newsPaperIn = 'pt-page-rotateInNewspaper',
+    newsPaperOut = 'pt-page-rotateOutNewspaper';
 
 var getDirection = function(prevUrl, currentUrl){
     var direction;
@@ -10,8 +12,8 @@ var getDirection = function(prevUrl, currentUrl){
 
     prevUrl = prevUrl || "";
     currentUrl = currentUrl || "";
-    prevUrl = '/' + prevUrl.split("/").slice(-1)[0];
-    currentUrl = '/' + currentUrl.split("/").slice(-1)[0];
+    prevUrl = prevUrl.replace(window.location.origin, '');
+    currentUrl = currentUrl.replace(window.location.origin, '');
 
     var prev = locations.indexOf(prevUrl);
     var current = locations.indexOf(currentUrl);
@@ -28,29 +30,43 @@ var getDirection = function(prevUrl, currentUrl){
     return direction;
 };
 
-var direction = getDirection(window.sessionStorage.prevUrl, window.location.pathname);
-var inClass = inClassRight;
-if(direction !== "left"){
-    inClass = inClassLeft;
+
+var inClass = newsPaperIn;
+if(window.sessionStorage.prevUrl !== window.location.pathname){
+    var direction = getDirection(window.sessionStorage.prevUrl, window.location.pathname);
+    if(direction){
+        inClass = inClassRight;
+        if(direction !== "left"){
+            inClass = inClassLeft;
+        }
+    }
 }
 $container.addClass(inClass).show();
+window.sessionStorage.prevUrl = window.location.pathname;
 
 $(function(){
 
     $("a[href!='/']").on('click', function(){
-        var newLocation = this.href;
-        if(newLocation && newLocation !== "#" && newLocation !== "javascript:void(0)"){
-
-            var direction = getDirection(window.location.pathname, newLocation);
-            outClass = outClassRight;
-            if(direction !== "left"){
-                outClass = outClassLeft;
+        var newLocation = (this.href || '').replace(window.location.origin, '');
+        if(newLocation && newLocation !== "#" && newLocation !== 'javascript:void(0)'){
+            if(newLocation === window.location.pathname)
+            {
+                window.location.reload();
             }
-            $container.removeClass(inClass).addClass(outClass);
-            window.sessionStorage.prevUrl = window.location.pathname;
-            setTimeout(function(){
-                window.location = newLocation;
-            }, 500);
+            else{
+                var direction = getDirection(window.location.pathname, newLocation);
+                var outClass = newsPaperOut;
+                if(direction){
+                    outClass = outClassRight;
+                    if(direction !== "left"){
+                        outClass = outClassLeft;
+                    }
+                }
+                $container.removeClass(inClass).addClass(outClass);
+                setTimeout(function(){
+                    window.location = newLocation;
+                }, 500);
+            }
         }
         return false;
     });
